cb065e5a6c8477d18881ec144173a773aaf9f0d0
Shu OGAWARA <expajp@gmail.com>
2018-09-20 21:32:56 +0900

    modified はじめに

--- はじめに/code/index.js
+++ はじめに/code/index.js
@@ -17,5 +17,47 @@ var tutorial = function(){
             seen: true
         }
     })
+    var app4 = new Vue({
+        el: '#app4',
+        data: {
+            todos: [
+                { text: 'Learn JavaScript' },
+                { text: 'Learn Vue' },
+                { text: 'Build something awesome' }
+            ]
+        }
+    })
+    var app5 = new Vue({
+        el: '#app5',
+        data: {
+            message: 'Hello Vue.js!'
+        },
+        methods: {
+            reverseMessage: function(){
+                this.message = this.message.split('').reverse().join('')
+            }
+        }
+    });
+    var app6 = new Vue({
+        el: '#app6',
+        data: {
+            message: 'Hello Vue.js!'
+        }
+    })
+
+    Vue.component('todo-item', {
+        props: ['todo'],
+        template: '<li>{{ todo.text }}</li>'
+    })
+    var app7 = new Vue({
+        el: '#app7',
+        data: {
+            groceryList: [
+                { id: 0, text: 'Vegetables' },
+                { id: 1, text: 'Cheese' },
+                { id: 2, text: 'Whatever else humans are supposed to eat' }
+            ]
+        }
+    })
 }
 window.onload = tutorial;
