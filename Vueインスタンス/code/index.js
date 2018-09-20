3795a10b01b866225e70ee1700dd82fcbf041d23
Shu OGAWARA <expajp@gmail.com>
2018-09-20 20:16:27 +0900

    Vueインスタンス - インスタンスライフサイクルフック ~ ライフサイクルダイヤグラム

--- Vueインスタンス/code/index.js
+++ Vueインスタンス/code/index.js
@@ -1,4 +1,11 @@
 var tutorial = function(){
-    
+    var vm = new Vue({
+        data: {
+            a: 1
+        },
+        created: function(){
+            console.log("a is: " + this.a)
+        }
+    })
 }
 window.onload = tutorial;
