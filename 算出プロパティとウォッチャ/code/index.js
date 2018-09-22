594eaf1e7f6f93720dfd5fd4cdb635f279cdeda3
Shu OGAWARA <expajp@gmail.com>
2018-09-22 11:43:30 +0900

    算出プロパティとウォッチャ - 算出プロパティvs.監視プロパティ ~ 算出Setter関数

--- 算出プロパティとウォッチャ/code/index.js
+++ 算出プロパティとウォッチャ/code/index.js
@@ -10,5 +10,41 @@ var tutorial = function(){
             }
         }
     })
+    var vm01 = new Vue({
+        el: "#example01",
+        data: {
+            firstName: "Foo",
+            lastName: "Bar",
+            fullName: "Foo Bar"
+        },
+        watch: {
+            firstName: function (val) {
+                this.fullName = val + ' ' + this.lastName
+            },
+            lastName: function (val) {
+            this.fullName = this.firstName + ' ' + val
+            }
+        }
+    })
+    var vm02 = new Vue({
+        el: "#example02",
+        data: {
+            firstName: "Foo",
+            lastName: "Bar"
+        },
+        computed: {
+            fullName: {
+                get: function(){
+                    return this.firstName + ' ' + this.lastName
+                },
+                set: function(newValue){
+                    var names = newValue.split(' ')
+                    this.firstName = names[0]
+                    this.lastName = names[names.length-1]
+                }
+            }
+        }
+
+    })
 }
 window.onload = tutorial;
