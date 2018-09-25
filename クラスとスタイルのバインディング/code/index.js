77a834a365fbedbaa7513ae3f70f0e2c7d8deb4b
Shu OGAWARA <expajp@gmail.com>
2018-09-26 00:09:01 +0900

    クラスとスタイルのバインディング ~ HTMLクラスのバインディング

--- クラスとスタイルのバインディング/code/index.js
+++ クラスとスタイルのバインディング/code/index.js
@@ -1,4 +1,19 @@
 var tutorial = function(){
-    
+    var vm01 = new Vue({
+        el: "#vm01",
+        data: {
+            red: false,
+            big: false
+        },
+        computed: {
+            classObject: function(){
+                return {
+                    red: this.red && !this.big,
+                    big: !this.red && this.big,
+                    italic: !this.red && !this.big
+                }
+            }
+        }
+    })
 }
 window.onload = tutorial;
