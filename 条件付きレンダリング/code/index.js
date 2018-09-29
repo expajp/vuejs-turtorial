ee389c6bfd983090059f0315bd0506bb7d2cbe82
Shu OGAWARA <expajp@gmail.com>
2018-09-29 23:56:40 +0900

    条件付きレンダリング - v-if ~ テンプレートでのv-ifによる条件グループ

---
+++ 条件付きレンダリング/code/index.js
@@ -0,0 +1,15 @@
+var tutorial = function(){
+    var vm01 = new Vue({
+        el: '#vm01',
+        data: {
+            ok: true
+        }
+    })
+    var vm02 = new Vue({
+        el: '#vm02',
+        data: {
+            ok: true
+        }
+    })
+}
+window.onload = tutorial;
