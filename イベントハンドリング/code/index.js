ce9213a91d8e7afdad77682570370c8be49952a0
Shu OGAWARA <expajp@gmail.com>
2018-10-08 17:32:00 +0900

    イベントハンドリング - メソッドイベントハンドラ

--- イベントハンドリング/code/index.js
+++ イベントハンドリング/code/index.js
@@ -4,6 +4,20 @@ var tutorial = function(){
         data: {
             counter: 0
         }
-    })    
+    })
+    var vm02 = new Vue({
+        el: '#vm02',
+        data: {
+            name: 'Vue.js'
+        },
+        methods: {
+            greet: function(event){
+                alert('Hello, '+this.name+'!!');
+                if(event) {
+                    alert(event);
+                }
+            }
+        }
+    })
 }
 window.onload = tutorial;
