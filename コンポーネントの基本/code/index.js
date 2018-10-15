88fe3da9b56488567a90afa62b1e2816eca6c755
Shu OGAWARA <expajp@gmail.com>
2018-10-16 08:52:06 +0900

    コンポーネントの基本 - コンポーネントの再利用 ~ プロパティを使用した子コンポーネントへのデータの受け渡し

--- コンポーネントの基本/code/index.js
+++ コンポーネントの基本/code/index.js
@@ -6,5 +6,20 @@ var tutorial = function(){
         template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
     })
     var vm01 = new Vue({ el: '#vm01' })
+
+    Vue.component('blog-post', {
+        props: ['title'],
+        template: '<h3>{{ title }}</h3>'
+    })
+    var vm02 = new Vue({ 
+        el: '#vm02', 
+        data: {
+            posts: [
+                { id: 1, title: "Philosopher's Stone" },
+                { id: 2, title: "Chamber Of Secrets" },
+                { id: 3, title: "Prizoner Of Azkaban" },
+            ]
+        }
+    })
 }
 window.onload = tutorial;
