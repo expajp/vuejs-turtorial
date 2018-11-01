3fcdab7a48766dfdc6ef13a25bec6e7545b0fd43
Shu OGAWARA <expajp@gmail.com>
2018-11-01 20:09:16 +0900

    カスタムイベント - コンポーネントにネイティブイベントをバインディング

--- コンポーネントの詳細/カスタムイベント/code/index.js
+++ コンポーネントの詳細/カスタムイベント/code/index.js
@@ -21,5 +21,43 @@ var tutorial = function(){
             lovingVue: true
         }
     })
+    Vue.component('base-input', {
+        inheritAttrs: false,
+        props: ['label', 'value'],
+        computed: {
+            inputListeners: function(){
+                var vm = this
+                // 複数のオブジェクトをObject.assignでマージ
+                return Object.assign({},
+                    // すべてのイベントリスナを追加
+                    this.$listeners,
+                    {
+                        // inputイベントを下に送ることでコンポーネントがv-modelと動作する
+                        input: function(event){
+                            vm.$emit('input', event.target.value)
+                        }
+                    }
+                )
+            }
+        },
+        template: `
+            <label>
+                {{ label }}
+                <input 
+                    v-bind="$attrs"
+                    v-bind:value="value"
+                    v-on="inputListeners"
+                />
+                {{ value }}
+            </label>
+        `
+    })
+    let vm02 = new Vue({
+        el: '#vm02',
+        data: {
+            label: 'message',
+            value: 'Hello, world!'
+        }
+    })
 }
 window.onload = tutorial;
