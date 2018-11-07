e9cd87469f49a5ec5ab99a9daf32b405ffe4a93c
Shu OGAWARA <expajp@gmail.com>
2018-11-07 19:51:44 +0900

    スロット - 名前付きスロット

--- コンポーネントの詳細/スロット/code/index.js
+++ コンポーネントの詳細/スロット/code/index.js
@@ -29,5 +29,35 @@ var tutorial = function(){
     let vm02 = new Vue({
         el: '#vm02'
     })
+    Vue.component('todo-list', {
+        props: ['todos'],
+        template: `
+        <ul>
+            <li 
+                v-for="todo in todos"
+                v-bind:key="todo.id"
+            >
+                <slot v-bind:todo="todo">
+                    {{ todo.text }}
+                </slot>
+            </li>
+        </ul>
+        `
+    })
+    let vm03 = new Vue({
+        el: '#vm03', 
+        data: {
+            doraemons: [
+                { id: 1, text: 'のび太の恐竜', isCompleted: false },
+                { id: 2, text: 'のび太の宇宙開拓史', isCompleted: false },
+                { id: 3, text: 'のび太の大魔境', isCompleted: false },
+            ],
+            pokemons: [
+                { id: 1, text: 'ミュウツーの逆襲', isCompleted: true },
+                { id: 2, text: 'ルギア爆誕', isCompleted: false },
+                { id: 3, text: '結晶塔の帝王', isCompleted: false },
+            ]
+        }
+    })
 }
 window.onload = tutorial;
