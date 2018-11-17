d932d2b391a0a4609dc1277fc49d6b1c13015ba2
Shu OGAWARA <expajp@gmail.com>
2018-11-17 11:51:33 +0900

    Enter/Leave とトランジション一覧 - JavaScriptフック

--- トランジションとアニメーション/Enter_Leaveとトランジション一覧/code/index.js
+++ トランジションとアニメーション/Enter_Leaveとトランジション一覧/code/index.js
@@ -23,5 +23,36 @@ var tutorial = function(){
             show: true
         }
     })
+    let vm05 = new Vue({
+        el: '#vm05',
+        data: {
+            show: true
+        },
+        methods: {
+            beforeEnter: function(el){
+                el.style.opacity = 0
+                el.style.fontSize = '0.5em'
+            },
+            enter: function(el, done){
+                Velocity(el, { opacity: 1 }, { duration: 300 })
+                Velocity(el, { fontSize: '1.5em' }, { duration: 300 })
+                Velocity(el, { fontSize: '1em' }, { complete: done })
+            },
+            leave: function(el, done){
+                Velocity(el, { translateX: '5px', rotateZ: '50deg' }, { duration: 600 })
+                Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+                Velocity(
+                    el, 
+                    { 
+                        rotateZ: '45deg',
+                        translateY: '10px',
+                        translateX: '10px',
+                        opacity: 0
+                    }, 
+                    { complete: done }
+                )
+            }  
+        }
+    })
 }
 window.onload = tutorial;
